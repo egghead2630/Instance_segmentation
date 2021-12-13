a44c584cfd0e7f5b9c96c1113b1a1ad58e1ec7f2
Hello WOrld! <egghead2612@gmail.com>
2021-12-13 19:02:49 +0800

    Add files via upload

---
+++ configs/HW.py
@@ -0,0 +1,29 @@
+_base_ = '../mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
+
+# We also need to change the num_classes in head to match the dataset's annotation
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=1),
+        mask_head=dict(num_classes=1)))
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('1',)
+data = dict(
+    train=dict(
+        img_prefix='dataset/',
+        classes=classes,
+        ann_file='dataset/train.json'),
+    val=dict(
+        img_prefix='dataset/',
+        classes=classes,
+        ann_file='dataset/vali.json'),
+    test=dict(
+        img_prefix='dataset/test/',
+        classes=classes,
+        ann_file='dataset/test.json'))
+
+load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
+#load_from = 'output/now_best/latest.pth'
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+# load_from = 'checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
