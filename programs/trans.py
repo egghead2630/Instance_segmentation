17bf27ae8da3a34ef09d5ca30cd5b2d62e8000ae
Hello WOrld! <egghead2612@gmail.com>
2021-12-14 21:10:13 +0800

    Add files via upload

---
+++ programs/trans.py
@@ -0,0 +1,24 @@
+f = open('test_img_ids.json', 'r')
+lines = []
+for l in f:
+    lines.append(l)
+
+f.close()
+
+
+f = open('test_img_ids.json', 'w')
+
+f.write('{\n')
+
+f.write('\"images\": ')
+for line in lines:
+    f.write(line)
+f.write(', \n')
+
+f.write('\"categories\": \n')
+
+f.write('[\n')
+f.write('{\"id\": 1 \"name\": 1}\n')
+f.write(']\n')
+
+f.write('}\n')
